bb5c32dcfc40b86d7cc1604ed870d5d7bdc47222
Danny Yoo <dyoo@cs.wpi.edu>
2011-09-02 16:40:31 -0400

    correcting names

--- client2.rkt
+++ client2.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname client2) (read-case-sensitive #t) (teachpacks ((lib "cs0190tpk.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "cs0190tpk.rkt" "installed-teachpacks")))))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname client2) (read-case-sensitive #t) (teachpacks ((lib "cs019-tpk.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "cs019-tpk.rkt" "installed-teachpacks")))))
 (require "client.rkt")
 
 (check-expect (hash-ref ht2 "x") 10)
